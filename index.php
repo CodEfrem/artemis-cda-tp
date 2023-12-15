03fa5465dbd2fb7ae882b6c608537a3fbeff5edd
Efrem YOUSSOUF <youssouf.efrem@gmail.com>
2023-12-15 09:09:09 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php 
+
+$pageTitle = 'Tableau de bord';
+
+include __DIR__ . '/templates/header.php';
+
+include __DIR__ . '/templates/hero.php';
+
+include __DIR__ . '/templates/last.php';
+
+include __DIR__ . '/templates/_partials/last_books.php';
+
+include __DIR__ . '/templates/footer.php';
